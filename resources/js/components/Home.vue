732fc212296d8e13a6b4d65b1b53e1c73acca28e
RenatoAndrade22 <renatoandradedesign@gmail.com>
2022-01-04 19:28:57 -0300

    First
    first

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>testando</h1>
+    </div>
+</template>
+<script>
+export default {
+
+    
+}
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,15 @@
+import Home from './components/Home'
+
+export default {
+    mode: 'history',
+    routes:[
+        {
+            path:'/',
+            component: Home
+        },
+        {
+            path:'/about',
+            component: Home
+        },
+    ]
+}
