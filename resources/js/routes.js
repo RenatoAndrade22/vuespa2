fbf77b7b00093b2aab9e6cf4efe84e1d6825fd11
renato <renatoandradedesign@gmail.com>
2022-01-04 22:19:21 -0300

    Routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,15 +1,23 @@
 import Home from './components/Home'
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 export default {
     mode: 'history',
+    linkActiveClass:'active-menu',
+
     routes:[
+        {
+            path:'*',
+            component: NotFound
+        },
         {
             path:'/',
             component: Home
         },
         {
             path:'/about',
-            component: Home
+            component: About
         },
     ]
 }
